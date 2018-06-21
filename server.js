8bf290a09ce182e0bd51adbf8cbc6c19e6b00603
Paul Suarez <paulsblogging.com@gmail.com>
2018-06-21 09:39:53 -0700

    fixed server file to work local and cloud

--- server.js
+++ server.js
@@ -12,21 +12,17 @@ const PORT = process.env.PORT;
 
 // Database Setup
 
-// // DONE: Don't forget to set your own conString
-// // conString for Travis' machine 
-// // const conString = 'postgres://postgres:1234@localhost:5432/books_app'; 
-// // constString for Paul's machine: 
-// // const conString = 'postgres://localhost:5432/books_app';
-
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // I had to change the below from the above AND add this to the server terminal: 
 // FOR PAUL:
 // export DATABASE_URL=postgres://localhost:5432/books_app
 // FOR TRAVIS:
 // export DATABASE_URL=postgres://postgres:1234@localhost:5432/books_app
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+// this is for cloud
 const client = new pg.Client(process.env.DATABASE_URL);
+
+
 client.connect();
 client.on('error', err => console.log(err));
 
@@ -35,8 +31,56 @@ app.use(cors());
 
 // API Endpoints
 app.get('/api/v1/books', (req, res) => {
-    console.log('One small step for man. One giant step for computer-kind (handling a GET request by a client)')
+    console.log('One small step for man. One giant step for computer-kind (handling a GET request by a client')
     let SQL = `SELECT * FROM books;`;
+    client.query(SQL)
+    .then(results => res.send(results.rows))
+    .catch(console.error);
+});
+
+app.get('/api/v1/books-limited', (req, res) => {
+    console.log('limited request')
+    let SQL = `
+        SELECT book_id, title, author, image_url FROM books;
+        `;
+    client.query(SQL)
+    .then(results => res.send(results.rows))
+    .catch(console.error);
+});
+
+app.post('/api/v1/books', (req, res) => {
+    let SQL = `INSERT INTO books_app(book_id, title, author, isbn, image_url, description)
+    VALUES ($1, $2, $3, $4, $5, $6);`;
+
+
+    let values = [
+        req.params.id,
+        req.body.title,
+        req.body.author,
+        req.body.isbn,
+        req.body.image_url,
+        req.body.description
+    ];
+
+    client.query(SQL, values)
+        .then(function () {
+        res.send('insert completed')
+        })
+        .catch(function (err) {
+            console.error(err);
+    })
+})
+
+
+app.get('/api/v1/books:id', (req, res) => {
+    console.log(req);
+    let SQL = `
+        SELECT * FROM books WHERE book_id=$1;
+        `;
+    
+        // let values = [request.body.book_id];
+    // let values = [body.book_id];
+
     client.query(SQL)
     .then(results => res.send(results.rows))
     .catch(console.error);
